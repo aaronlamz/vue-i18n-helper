621555dd43ff04cfd44dd4ef4ac225930aea7ed1
Aaronlamz <qylinxia@qq.com>
2022-02-28 22:55:43 +0800

    docs: update

--- src/extension.ts
+++ src/extension.ts
@@ -1,13 +1,10 @@
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
 export function activate(context: vscode.ExtensionContext) {
 	console.log('Congratulations, your extension "vscode-i18n-helper" is now active!');
-
 	let disposable = vscode.commands.registerCommand('vscode-i18n-helper.helloWorld', () => {
 		vscode.window.showInformationMessage('Hello World from vscode-i18n-helper!');
 	});
-
 	context.subscriptions.push(disposable);
 }
 
