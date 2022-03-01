a3056245280f4a3db3457fa07fc1ec069e632af0
Aaronlamz <qylinxia@qq.com>
2022-03-01 15:44:30 +0800

    chore: add prettier

--- src/extension.ts
+++ src/extension.ts
@@ -1,14 +1,19 @@
-import * as vscode from 'vscode';
+import * as vscode from 'vscode'
 
 export function activate(context: vscode.ExtensionContext) {
-	console.log('Congratulations, your extension "vscode-i18n-helper" is now active!');
-	let disposable = vscode.commands.registerCommand('vscode-i18n-helper.helloWorld', () => {
-		vscode.window.showInformationMessage('Hello World from vscode-i18n-helper!');
-	});
-	context.subscriptions.push(disposable);
+  console.log('"vscode-i18n-helper" is now active!')
+  const disposable = vscode.commands.registerCommand(
+    'vscode-i18n-helper.helloWorld',
+    () => {
+      vscode.window.showInformationMessage(
+        'Hello World from vscode-i18n-helper!'
+      )
+    }
+  )
+
+  context.subscriptions.push(disposable)
 }
 
 export function deactivate() {
-  console.log('Deactivated');
+  console.log('Deactivated')
 }
-
