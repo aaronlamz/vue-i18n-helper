553c0dda570a43d6f523995bce053cdd6ac7e3ef
Aaronlamz <qylinxia@qq.com>
2022-03-02 20:51:44 +0800

    add texteditor replace

--- src/commands/extract.ts
+++ src/commands/extract.ts
@@ -3,6 +3,16 @@ import meta from '../meta'
 
 const commandExtarct = () => {
   vscode.window.showInformationMessage('excute extract commmand')
+  // replace selected content
+  // TODO: load data by i18n file
+  const key = 'key'
+  // The currently active editor or undefined
+  if (vscode.window.activeTextEditor) {
+    vscode.window.activeTextEditor.edit(editBuilder => {
+      const { start, end } = vscode.window.activeTextEditor?.selection
+      editBuilder.replace(new vscode.Range(start, end), key)
+    })
+  }
 }
 
 export const extract = () => {
