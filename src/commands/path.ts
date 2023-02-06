0f9074d0e14b8ba69cd99c04dd10d73f722ea876
aaronlamz <aaronlamz2022@gmail.com>
2023-02-06 09:36:52 +0800

    update

--- src/commands/path.ts
+++ src/commands/path.ts
@@ -3,34 +3,44 @@ import * as fg from 'fast-glob'
 import meta from '../meta'
 import Config from '../config'
 
-const initPath = async () => {
-  const rootPath = vscode.workspace.rootPath
-  const pattern = [
-    `${rootPath}/**/(locales|locale|lang|langs|i18n|i18n-messages)`
-  ]
-  const result: any[] = await fg(pattern, {
-    ignore: ['**/node_modules'],
-    onlyDirectories: true
-  })
-  if (result.length) {
-    Config.updateI18nPaths(result)
-    const info = `${
-      Config.extensionName
-    }:已初始化以下语言包目录文件\n ${result.join('\n')}`
-    vscode.window.showInformationMessage(info)
-  } else {
-    vscode.window.showInformationMessage(
-      '初始化语言包目录失败，请检查语言包目录是否正确？或者自定义配置语言目录。'
-    )
+class I18nPath {
+  constructor() {
+    if (Config.hasI18nPaths) {
+      return
+    }
+    this.autoInitPath()
+  }
+
+  async autoInitPath() {
+    const rootPath = vscode.workspace.rootPath
+    const pattern = [
+      `${rootPath}/**/(locales|locale|lang|langs|i18n|i18n-messages)`
+    ]
+    const result: any[] = await fg(pattern, {
+      ignore: ['**/node_modules'],
+      onlyDirectories: true
+    })
+    if (result.length) {
+      Config.updateI18nPaths(result)
+      const info = `${
+        Config.extensionName
+      }:已初始化以下语言包目录文件\n ${result.join('\n')}`
+      vscode.window.showInformationMessage(info)
+    } else {
+      vscode.window.showInformationMessage(
+        '初始化语言包目录失败，请检查语言包目录是否正确？或者自定义配置语言目录。'
+      )
+    }
   }
 }
 
-// initialize default i18n path
-initPath()
+const i18nPath = new I18nPath()
 
 // default i18n path
-export const initPathCommand = () => {
-  return vscode.commands.registerCommand(meta.COMMANDS.initPath, initPath)
+export const autoInitPathCommand = () => {
+  return vscode.commands.registerCommand(meta.COMMANDS.autoInitPath, () => {
+    i18nPath.autoInitPath()
+  })
 }
 
 // customize i18n path
